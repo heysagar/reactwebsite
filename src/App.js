b806635055e1ae7a30ad32072a95907d4b50d933
Sagar Tripathi <sgr.bzns@gmail.com>
2021-05-09 20:35:52 +0530

    initial commit

--- src/App.js
+++ src/App.js
@@ -7,7 +7,7 @@ function App() {
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
         <p>
-         New App for github & Netlify. changes for build
+         New App for github & Netlify. changes for build. done on netlify
         </p>
       
       </header>
